dab8795c855278e7236612241c09c5436418996a
ryasik <ryasik.is@gmail.com>
2020-11-07 22:08:18 +0200

    change readme

--- src/utils/async.handler.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = callback => async (req, res, next) => {
-  try {
-    return await callback(req, res, next);
-  } catch (err) {
-    return next(err);
-  }
-};
-
-module.exports = asyncHandler;
